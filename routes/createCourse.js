29fd6a58f965f61d7db3f22deb5cf21e05caa1fc
Slack Unfair <aaa4bbb@trash-mail.com>
2014-05-06 18:13:08 +0200

    Added example for testing logged in users

--- routes/createCourse.js
+++ routes/createCourse.js
@@ -3,7 +3,12 @@
  */
 
 exports.display = function(req, res){
-  res.render('createCourse', { title: 'PASS' });
+  if(req.session.user != null){
+    res.render('createCourse', { title: 'PASS' });
+  }
+  else {
+    res.render('index',{title: 'USER REQUIRED' });
+  }
 };
 
 exports.post = function(req, res){

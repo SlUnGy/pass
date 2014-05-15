26b056d2e720095e670a7d56d96c406dc276d3f6
Slack Unfair <aaa4bbb@trash-mail.com>
2014-05-15 18:06:09 +0200

    Added choose student

--- routes/chooseCourse.js
+++ routes/chooseCourse.js
@@ -10,9 +10,27 @@ exports.display = function(req, res){
 		else {
 			var names = new Array();
 			for( var i=0; i<data.length; i++){
-				names.push(data[i]["title"]);
+				names.push(data[i]['title']);
 			}
 			res.render('chooseCourse', { title: 'PASS', courses: names });
 		}
 	});
 };
+
+exports.displayPost = function(req, res){
+	schemes.Course.findOne({title: req.body.courseNames},function(err, data){
+		if(err){
+			return console.error(err);
+		}
+		if(data != null){
+			var names = new Array();
+			for( var i=0; i<data.students.length; ++i){
+				names.push(data.students[i]['name']);
+			}
+			res.render('chooseStudent', { title: 'PASS', students: names });
+		}
+		else {
+			return console.error('No data found for selected course');
+		}
+	});
+}

6fb2e6fb6d1a57e799f3cdb7fa2a975305d1fefb
Slack Unfair <aaa4bbb@trash-mail.com>
2014-05-15 16:40:56 +0200

    Added retrieval and displayment of course information

--- routes/chooseCourse.js
+++ routes/chooseCourse.js
@@ -3,5 +3,16 @@ var db = require('../db');
 
 
 exports.display = function(req, res){
-	res.render('chooseCourse', { title: 'PASS'});
+	schemes.Course.find({}, function(err,data){
+		if(err){
+			return console.error(err);
+		}
+		else {
+			var names = new Array();
+			for( var i=0; i<data.length; i++){
+				names.push(data[i]["title"]);
+			}
+			res.render('chooseCourse', { title: 'PASS', courses: names });
+		}
+	});
 };

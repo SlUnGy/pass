fd9685539f4d4661111c9e78ceba466c43b0dc53
Slack Unfair <aaa4bbb@trash-mail.com>
2014-05-23 12:30:16 +0200

    Added assessment choosing

---
+++ routes/chooseAssessment.js
@@ -0,0 +1,25 @@
+var schemes = require('../DBschemes');
+var db = require('../db');
+
+exports.display = function(req, res){
+	asses = new Array();
+	asses.push('null');
+	res.render('chooseAssessment', {title: 'PASS', studentName: 'null', assessments: asses });
+};
+
+
+exports.displayPost = function(req, res){
+	schemes.Assessment.find({}, function(err,data){
+		if(err){
+			return console.error(err);
+		}
+		else {
+			var names = new Array();
+			for( var i=0; i<data.length; i++){
+				names.push(data[i]['name']);
+			}
+			res.render('chooseAssessment', { title: 'PASS', studentName: req.body.studentNames, assessments: names });
+		}
+	});
+}
+

163c1075fc856d7190e9290861e09b01d18c7bf4
Slack Unfair <aaa4bbb@trash-mail.com>
2014-05-24 18:44:54 +0200

    Fixed math calls

--- scoreFunctions.js
+++ scoreFunctions.js
@@ -1,7 +1,7 @@
 var util = require('./gamma.js')
 
 exports.profile2score = function(profileArray, tolerance){
-	var n=count(profileArray);
+	var n=profileArray.length;
 	var pLower=array(0);
 	var pUpper=array(0);
 	for(i=1;i<=n;i++){
@@ -29,37 +29,37 @@ exports.score2rate = function(score, standart, impact){
 	if(standart==0 || standart==1 || impact==0 ){
 		return 1;
 	} else if (score<(standart/(1-impact*(1-standart)))) {
-		return log(1 - score * (1 - impact * (1 - standart)) / standart) / log(impact * (1 - standart));
+		return Math.log(1 - score * (1 - impact * (1 - standart)) / standart) / Math.log(impact * (1 - standart));
 	}  else {
 		return -1;
 	}
 }
 
 exports.rate2score = function(baseline, rateImpactArray){
-	var n=count(rateImpactArray);
+	var n=rateImpactArray.length;
 	for(i=0;i<=n-1;i++){
 		if(rateImpactArray[i][0]==-1){
 			baseline=baseline/(1-rateImpactArray[i][1]*(1-baseline));
 		}else {
-			baseline=baseline*(1-pow((rateImpactArray[i][1]*(1-baseline)), rateImpactArray[i][0]))/(1-rateImpactArray[i][1]*(1-baseline));
+			baseline=baseline*(1-Math.pow((rateImpactArray[i][1]*(1-baseline)), rateImpactArray[i][0]))/(1-rateImpactArray[i][1]*(1-baseline));
 		}
 	}
 	return baseline;
 }
 
 exports.scores2score = function(lenience, scoreWeightArray){
-	var n=count(scoreWeightArray);
+	var n=scoreWeightArray.length;
 	var min=1;
 	var max=1;
 	for(i=0;i<=n-1;i++){
-		min=min*pow((1-scoreWeightArray[i][0]), scoreWeightArray[i][1]);
-		max=max*pow(scoreWeightArray[i][0], scoreWeightArray[i][1]);
+		min=min*Math.pow((1-scoreWeightArray[i][0]), scoreWeightArray[i][1]);
+		max=max*Math.pow(scoreWeightArray[i][0], scoreWeightArray[i][1]);
 	}
 	return (1 - lenience) * min + lenience * max;            
 }
 
 exports.score2grade = function(score, curvature, polarity, gMin, gMax){
-	curvature= pow((1-pow(score, curvature)), (1/curvature));
+	curvature= Math.pow((1-Math.pow(score, curvature)), (1/curvature));
 	g = polarity*curvature+(1-polarity)*(1-curvature);
 	return g * gMin + (1-g)*gMax; 
 }
